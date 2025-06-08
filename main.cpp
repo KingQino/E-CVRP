3c04b005f48dd462fe4d4e97bc52f61554fd3546
Yinghao Qin <yh.qin@foxmail.com>
2025-06-08 12:18:58 +0100

    init CMakeLists.txt

---
+++ main.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Yinghao Qin on 08/06/2025.
+//
---
+++ tests/test_main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Yinghao Qin on 08/06/2025.
+//
+#include "gtest/gtest.h"
+
+using namespace ::testing;
+using namespace std;
+
+
+int main(int argc, char **argv) {
+    InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
