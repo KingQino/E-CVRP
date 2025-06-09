31e4f7fc7dc4cfa744fae4f552b14e6a27ee9fae
Yinghao Qin <yh.qin@foxmail.com>
2025-06-09 20:33:44 +0100

    modify tests

--- tests/command_line_test.cpp
+++ tests/command_line_test.cpp
@@ -5,38 +5,39 @@
 #include "gtest/gtest.h"
 #include "command_line.hpp"
 
-using namespace ::testing;
+#include <filesystem>
 
+namespace fs = std::filesystem;
 
 TEST(CommandLineTest, ParseParametersPathOutput) {
-    char actual_path[PATH_MAX];
-    if (realpath("/Users/yhq/CLionProjects/E-CVRP/cmake-build-debug-llvm/Tests", actual_path) == nullptr) {
-        FAIL() << "Failed to resolve real path for /proc/self/exe";
-    }
+    // 模拟可执行文件路径，必须是有效路径，否则 CommandLine 会报错
+    fs::path fake_executable = fs::current_path() / "Run";
+
+    const std::string fake_exe_str = fake_executable.string();
 
     const char* argv[] = {
-        actual_path,
+        fake_exe_str.c_str(),  // argv[0]
         "-alg", "Lahc",
-         "-ins", "large.evrp",
-         "-log", "1",
-         "-stp", "0",
-         "-mth", "0",
+        "-ins", "large.evrp",
+        "-log", "1",
+        "-stp", "0",
+        "-mth", "0",
     };
     constexpr int argc = std::size(argv);
 
     CommandLine cmd(argc, const_cast<char**>(argv));
     Parameters params;
 
-    internal::CaptureStdout();
+    ::testing::internal::CaptureStdout();
     cmd.parse_parameters(params);
-    std::string output = internal::GetCapturedStdout();
+    std::string output = ::testing::internal::GetCapturedStdout();
 
     EXPECT_EQ(params.algorithm, Algorithm::LAHC);
     EXPECT_EQ(params.instance, "large.evrp");
-    EXPECT_EQ(params.enable_logging, true);
+    EXPECT_TRUE(params.enable_logging);
     EXPECT_EQ(params.stop_criteria, 0);
-    EXPECT_EQ(params.enable_multithreading, false);
+    EXPECT_FALSE(params.enable_multithreading);
 
-    EXPECT_TRUE(output.find("[INFO] Data path:") != std::string::npos);
-    EXPECT_TRUE(output.find("[INFO] Stats path:") != std::string::npos);
+    EXPECT_TRUE(output.find("[INFO] Default data path:") != std::string::npos);
+    EXPECT_TRUE(output.find("[INFO] Default stats path:") != std::string::npos);
 }
