35a6e775e85b6892b997e2cc0501c1842aac63d9
Yinghao Qin <yh.qin@foxmail.com>
2025-07-05 08:51:27 +0100

    update cmd output

--- main.cpp
+++ main.cpp
@@ -81,6 +81,10 @@ int main(int argc, char* argv[]) {
         std::cout << std::fixed << std::setprecision(2) << perf_of_trials[0] << std::endl;
     } else {
         Indicators indicators = StatsInterface::calculate_statistical_indicators(perf_of_trials);
+        if (indicators.min < 1e-6) {
+            std::cout << std::fixed << std::setprecision(2) << 99999999.99 << std::endl;  // If the minimum value is less than 1e6, print a large number
+            return 0;
+        }
         std::cout << std::fixed << std::setprecision(2) << indicators.avg << std::endl;
     }
 
